f29fd5474262ca4a7bccf6948ecdd1e85d2da067
Karthick Ramesh Kumar <karthickraster>
2024-08-30 17:14:25 +0530

    function call

--- src/App.jsx
+++ src/App.jsx
@@ -43,7 +43,7 @@ function App() {
         onChange={(e) => setSetsearchMovie(e.target.value)}
       />
       <button
-        onClick={getMovie}
+        onClick={() => getMovie}
         className="bg-blue-800 text-blue-200 px-4 py-2 rounded-3xl"
       >
         Search
