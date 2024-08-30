e160cefb1bbeabc2bb90538735b9eaec1ed16e13
Karthick Ramesh Kumar <karthickraster>
2024-08-30 19:15:15 +0530

    UI Fixes 1

--- src/App.jsx
+++ src/App.jsx
@@ -49,7 +49,7 @@ function App() {
       </h1>
       <div className="flex flex-col sm:flex-row items-center justify-center mb-5">
         <input
-          className="w-full mb-4 sm:mb-0 sm:w-2/4 lg:w-4/5 border-blue-800 border rounded-3xl px-2 py-2 sm:mr-3 "
+          className="w-full mb-4 sm:mb-0 sm:w-2/4 lg:w-4/5 border-blue-800 focus-within:border-lime-400 focus-visible:outline-lime-400 border rounded-3xl px-6 py-2 sm:mr-3 "
           type="text"
           placeholder="Filter"
           value={searchMovie}
@@ -57,13 +57,13 @@ function App() {
         />
         <button
           onClick={() => getMovie()}
-          className="w-full sm:w-1/3 mb-4 sm:mb-0  bg-blue-800 text-blue-200 px-4 py-2 rounded-3xl"
+          className="w-full sm:w-1/3 mb-4 sm:mb-0  bg-blue-800  text-blue-200  hover:bg-lime-400 hover:text-lime-800 ease-in transition-all	duration-300 px-4 py-2 rounded-3xl"
         >
           Search
         </button>
 
         <button
-          className="w-full sm:w-1/3 mb-4 sm:mb-0 sm:ml-2 bg-blue-800 text-blue-200 py-2 px-4 rounded-3xl "
+          className="w-full sm:w-1/3 mb-4 sm:mb-0 sm:ml-2 bg-blue-800 text-blue-200   hover:bg-lime-400 hover:text-lime-800 ease-in transition-all	duration-300 py-2 px-4 rounded-3xl "
           onClick={() => handleAscending()}
         >
           A-Z
@@ -96,7 +96,7 @@ function App() {
                 <div>
                   <img
                     src={e.Poster}
-                    className="object-fill h-[40vh] lg:h-[45vh] xl:h-[50vh] w-full"
+                    className=" object-fill h-[40vh] lg:h-[45vh] xl:h-[50vh] w-full"
                     alt={e.Title}
                   />
                   <h3
